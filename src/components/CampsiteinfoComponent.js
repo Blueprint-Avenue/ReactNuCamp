214ae8de4e6172511e9d618b0a1e9e6d4941ea48
Samiya <skmcintosh20@gmail.com>
2022-01-24 15:36:02 -0800

    added presentational components

--- src/components/CampsiteinfoComponent.js
+++ src/components/CampsiteinfoComponent.js
@@ -40,14 +40,15 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-                    <div className="row" >
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
                     </div>
-            )
-        } else {
-            return (<div> </div>)
+                </div>
+            );
         }
+        return <div />;
     }
 }
 export default CampsiteInfo;
