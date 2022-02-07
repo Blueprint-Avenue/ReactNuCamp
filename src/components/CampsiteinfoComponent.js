22727a9fe9f2c375d8132868b06a513e5e823805
Samiya <skmcintosh20@gmail.com>
2022-02-07 11:15:10 -0800

    Redux Actions

--- src/components/CampsiteinfoComponent.js
+++ src/components/CampsiteinfoComponent.js
@@ -17,7 +17,7 @@ function RenderCampsite({ campsite }) {
         </div>
     );
 };
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -37,7 +37,7 @@ function RenderComments({ comments }) {
                         )
                     )
                 }
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
             </div>
         )
     } else {
@@ -60,7 +60,7 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments} addComment={props.addComment} campsiteId={props.campsite.id} />
 
 
                 </div>
@@ -89,6 +89,7 @@ class CommentForm extends Component {
         this.toggleModal();
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
 
